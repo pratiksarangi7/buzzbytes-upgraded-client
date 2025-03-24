914b173815d539c68b538be1719a9ae07c0b355e
Pratik <sarangipratikcode@gmail.com>
2025-03-24 16:44:57 +0530

    minor change

--- clients/api.ts
+++ clients/api.ts
@@ -2,8 +2,6 @@ import { GraphQLClient } from "graphql-request";
 import { headers } from "next/headers";
 
 const isClient = typeof window !== "undefined";
-console.log(isClient);
-console.log(process.env.NEXT_PUBLIC_API_URL);
 
 export const graphQLClient = new GraphQLClient(
   process.env.NEXT_PUBLIC_API_URL as string,
